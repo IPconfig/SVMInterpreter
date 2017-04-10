45d9f1dedb91fb273e28ac33756da7f38d4fe00a
Justin de Haan <justin.haan92@gmail.com>
2017-04-10 22:37:28 +0200

    Set and Get Registers

--- src/Core.hs
+++ src/Core.hs
@@ -28,5 +28,23 @@ emptySVMState = SVMState
     , register4 = INT 0
     , programCounter = 0
     } 
+--setRegWithAnyArgument :: Register -> Literal -> SVM
+
+-- usage:: setRegister Reg1 (STRING("set Reg 1 to this text")) emptySVMState
+setRegister :: Register -> Value -> SVMState -> SVMState
+setRegister reg value svm = case reg of
+    Reg1 -> svm { register1 = value }
+    Reg2 -> svm { register2 = value }
+    Reg3 -> svm { register3 = value }
+    Reg4 -> svm { register4 = value }
+
+
+getRegister :: Register -> SVMState -> Value
+getRegister reg svm = case reg of
+  Reg1 -> register1 svm
+  Reg2 -> register2 svm
+  Reg3 -> register3 svm
+  Reg4 -> register4 svm
+
 createMemory :: Int -> Memory
 createMemory n = replicate n (INT 0)
