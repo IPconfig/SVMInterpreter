a55b3b6ae3d268b002fa83c03a25fc368de0a0d4
Justin de Haan <justin.haan92@gmail.com>
2017-04-12 03:51:38 +0200

    Evaluate Not instruction

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -10,3 +10,6 @@ eval instruction svm = case instruction of
     (LitAdress (LitRegister x)) -> trySetMemFromReg x right svm
     (LitRegister x) -> setRegWithAnyArg x right svm
     _ -> error "Invalid MOV structure"
+  Not reg -> case getRegister reg svm of
+    (INT x) ->  if x >= 0 then setRegister reg (INT (-1)) svm else setRegister reg (INT 0) svm
+    _ -> error "NOT accepts only integer values"
--- test/InterpreterSpec.hs
+++ test/InterpreterSpec.hs
@@ -30,4 +30,6 @@ spec =
         eval (Mov (LitAdress (LitRegister Reg4)) (LitString "test")) testSVMState `shouldBe` SVMState {memory = [STRING "test", DOUBLE 1.0,STRING "3",INT 4], register1 = INT 1, register2 = DOUBLE 2.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
       it "with register" $ do
         eval (Mov (LitAdress (LitRegister Reg1)) (LitString "test") ) testSVMState `shouldBe` SVMState {memory = [INT 0, STRING "test",STRING "3",INT 4], register1 = INT 1, register2 = DOUBLE 2.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
+    it "evaluate Not" $ do
+      eval (Not Reg1) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT (-1), register2 = DOUBLE 2.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
       
