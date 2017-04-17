28273bc7b59d05a55258057897a626b673d2b148
Justin de Haan <justin.haan92@gmail.com>
2017-04-18 00:15:06 +0200

    rename arguments Mov Evaluation

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -5,10 +5,10 @@ import ADT
 eval :: Instruction -> SVMState -> SVMState
 eval instruction svm = case instruction of
   Nop -> updateProgramCounter svm
-  Mov left right -> case left of -- Mov (LitAdress(LitInt 10)) (LitInt 5)
-    (LitAdress (LitInt x)) -> setMemWithAnyArg x right svm
-    (LitAdress (LitRegister x)) -> trySetMemFromReg x right svm
-    (LitRegister x) -> setRegWithAnyArg x right svm
+  Mov arg1 arg2 -> case arg1 of -- Mov (LitAdress(LitInt 10)) (LitInt 5)
+    (LitAdress (LitInt x)) -> setMemWithAnyArg x arg2 svm
+    (LitAdress (LitRegister x)) -> trySetMemFromReg x arg2 svm
+    (LitRegister x) -> setRegWithAnyArg x arg2 svm
     _ -> error "Invalid MOV structure"
   Not reg -> case getRegister reg svm of
     (INT x) ->  if x >= 0 then setRegister reg (INT (-1)) svm else setRegister reg (INT 0) svm
