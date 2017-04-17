c386c30a316d3b900162f85ff8bc67fe1436cec4
Justin de Haan <justin.haan92@gmail.com>
2017-04-18 00:15:54 +0200

    add evaluation and tests for Binary Operator instructions
    And, Or, Mod, Add, Sub, Mul, Div, Cmp

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -10,6 +10,45 @@ eval instruction svm = case instruction of
     (LitAdress (LitRegister x)) -> trySetMemFromReg x arg2 svm
     (LitRegister x) -> setRegWithAnyArg x arg2 svm
     _ -> error "Invalid MOV structure"
+  And arg1 arg2 -> case getBinaryValues arg1 arg2 svm of
+    ((INT x), (INT y)) -> if x >= 0 && y>= 0 then setRegister arg1 (INT 1) svm else setRegister arg1 (INT (-1)) svm
+    _ -> error "AND can be applied only to integer values"
+  Or arg1 arg2 -> case getBinaryValues arg1 arg2 svm of
+    ((INT x), (INT y)) -> if x < 0 && y < 0 then setRegister arg1 (INT (-1)) svm else setRegister arg1 (INT 1) svm
+    _ -> error "OR can be applied only to integer values"
   Not reg -> case getRegister reg svm of
     (INT x) ->  if x >= 0 then setRegister reg (INT (-1)) svm else setRegister reg (INT 0) svm
-    _ -> error "NOT accepts only integer values"
+    _ -> error "NOT accepts only integer values in register"
+  Mod arg1 arg2 -> case getBinaryValues arg1 arg2 svm of
+    ((INT x), (INT y)) -> setRegister arg1 (INT (x `mod` y)) svm
+    _ -> error "MOD can be applied only to integer values"
+  Add arg1 arg2 -> case getBinaryValues arg1 arg2 svm of
+    ((INT x), (INT y)) -> setRegister arg1 (INT (x + y)) svm
+    ((DOUBLE x), (DOUBLE y)) -> setRegister arg1 (DOUBLE (x + y)) svm
+    ((STRING x), (STRING y)) -> setRegister arg1 (STRING (x ++ y)) svm
+    _ -> error "ADD accepts only integer, double or strings as arguments"
+  Sub arg1 arg2 -> case getBinaryValues arg1 arg2 svm of
+    ((INT x), (INT y)) -> setRegister arg1 (INT (x - y)) svm
+    ((DOUBLE x), (DOUBLE y)) -> setRegister arg1 (DOUBLE (x - y)) svm
+    _ -> error "SUB accepts only integer or double as arguments"
+  Mul arg1 arg2 -> case getBinaryValues arg1 arg2 svm of
+    ((INT x), (INT y)) -> setRegister arg1 (INT (x * y)) svm
+    ((DOUBLE x), (DOUBLE y)) -> setRegister arg1 (DOUBLE (x * y)) svm
+    _ -> error "MUL accepts only integer or double as arguments"
+  Div arg1 arg2 -> case getBinaryValues arg1 arg2 svm of
+    ((INT x), (INT y)) -> setRegister arg1 (INT (x `div` y)) svm -- `div` can only be applied to integral numbers
+    ((DOUBLE x), (DOUBLE y)) -> setRegister arg1 (DOUBLE (x / y)) svm -- (/) only can be applied to fractional numbers
+    _ -> error "DIV accepts only integer or double as arguments"
+  Cmp arg1 arg2 -> case getBinaryValues arg1 arg2 svm of
+    ((INT x), (INT y)) -> compareFunc x y -- `div` can only be applied to integral numbers
+    ((DOUBLE x), (DOUBLE y)) -> compareFunc x y -- (/) only can be applied to fractional numbers
+    _ -> error "Cmp accepts only integer or double as arguments" 
+    where 
+      compareFunc x y 
+        | x < y     = setRegister arg1 (INT (-1)) svm
+        | x == y    = setRegister arg1 (INT 0) svm
+        | otherwise = setRegister arg1 (INT 1) svm
+  Label label-> svm -- do Nothing
+  Jmp label -> svm
+  Jc label register -> svm
+  Jeq label register -> svm
--- test/InterpreterSpec.hs
+++ test/InterpreterSpec.hs
@@ -30,6 +30,40 @@ spec =
         eval (Mov (LitAdress (LitRegister Reg4)) (LitString "test")) testSVMState `shouldBe` SVMState {memory = [STRING "test", DOUBLE 1.0,STRING "3",INT 4], register1 = INT 1, register2 = DOUBLE 2.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
       it "with register" $ do
         eval (Mov (LitAdress (LitRegister Reg1)) (LitString "test") ) testSVMState `shouldBe` SVMState {memory = [INT 0, STRING "test",STRING "3",INT 4], register1 = INT 1, register2 = DOUBLE 2.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
+    it "evaluate And" $ do
+      eval (And Reg4 (LitInt 5)) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT 1, register2 = DOUBLE 2.0, register3 = STRING "reg3", register4 = INT 1, programCounter = 0}
+    it "evaluate Or" $ do
+      eval (Or Reg4 (LitInt 5)) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT 1, register2 = DOUBLE 2.0, register3 = STRING "reg3", register4 = INT 1, programCounter = 0}
     it "evaluate Not" $ do
       eval (Not Reg1) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT (-1), register2 = DOUBLE 2.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
-      
+    it "evaluate Mod" $ do
+      eval (Mod Reg1 (LitInt 5)) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT 1, register2 = DOUBLE 2.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
+    describe "evaluate Add" $ do
+        it "with integers" $ do
+          eval (Add Reg1 (LitInt 5)) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT 6, register2 = DOUBLE 2.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
+        it "with doubles" $ do
+          eval (Add Reg2 (LitFloat 5.0)) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT 1, register2 = DOUBLE 7.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
+        it "with strings" $ do
+          eval (Add Reg3 (LitString "added")) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT 1, register2 = DOUBLE 2.0, register3 = STRING "reg3added", register4 = INT 0, programCounter = 0}
+    describe "evaluate Sub" $ do
+      it "with integers" $ do
+        eval (Sub Reg1 (LitInt 5)) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT (-4), register2 = DOUBLE 2.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
+      it "with doubles" $ do
+        eval (Sub Reg2 (LitFloat 1.0)) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT 1, register2 = DOUBLE 1.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
+    describe "evaluate Mul" $ do
+      it "with integers" $ do
+        eval (Mul Reg1 (LitInt 5)) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT 5, register2 = DOUBLE 2.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
+      it "with doubles" $ do
+        eval (Mul Reg2 (LitFloat 3.0)) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT 1, register2 = DOUBLE 6.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
+    describe "evaluate Div" $ do
+      it "with integers" $ do
+        eval (Div Reg1 (LitInt 5)) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT 0, register2 = DOUBLE 2.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
+      it "with doubles" $ do
+        eval (Div Reg2 (LitFloat 2.0)) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT 1, register2 = DOUBLE 1.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
+    describe "evaluate Cmp" $ do
+      it "with x < y" $ do
+        eval (Cmp Reg1 (LitInt 5)) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT (-1), register2 = DOUBLE 2.0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
+      it "with x == y" $ do
+        eval (Cmp Reg2 (LitFloat 2.0)) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT 1, register2 = INT 0, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
+      it "with x > y" $ do
+        eval (Cmp Reg2 (LitFloat 1.0)) testSVMState `shouldBe` SVMState {memory = [INT 0, DOUBLE 1.0,STRING "3",INT 4], register1 = INT 1, register2 = INT 1, register3 = STRING "reg3", register4 = INT 0, programCounter = 0}
